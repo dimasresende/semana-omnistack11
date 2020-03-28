dd803c39a3ec410fec0245d44348e3d765eaa607
Dimas Resende <dimas.resende@gmail.com>
2020-03-28 18:33:48 -0300

    Integracao de logout

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -11,6 +11,8 @@ export default function Profile() {
     const [incidents, setIncidents] = useState([]);
     const ongID = localStorage.getItem('ongID');
     const ongName = localStorage.getItem('ongName');
+
+    const history = useHistory();
 
     useEffect(() => {
         api.get('profile', {
@@ -33,13 +35,18 @@ export default function Profile() {
         }
     }
 
+    function handleLogout() {
+        localStorage.clear();
+        history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be The Hero"/>
                 <span>Bem vinda, {ongName}</span>
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#e02041" />
                 </button>
             </header>
