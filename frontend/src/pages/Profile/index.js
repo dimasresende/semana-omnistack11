f47b1079d97de4ebac6397a13468c38b737e47c3
Dimas Resende <dimas.resende@gmail.com>
2020-03-28 16:55:02 -0300

    Integracao da pagina de incidentes por usuario

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 import logoImg from '../../assets/logo.svg';
 
 export default function Profile() {
+    const [incidents, setIncidents] = useState([]);
+    const ongID = localStorage.getItem('ongID');
+    const ongName = localStorage.getItem('ongName');
+
+    useEffect(() => {
+        api.get('profile', {
+            headers: { Authorization: ongID }
+        }).then(response => {
+            setIncidents(response.data);
+        })
+    }, [ongID]); // O array indica qual valor ele 'escuta' para executar, se estiver vazio executa só uma vez.
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be The Hero"/>
-                <span>Bem vinda, APAD</span>
+                <span>Bem vinda, {ongName}</span>
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
                 <button type="button">
                     <FiPower size={18} color="#e02041" />
@@ -18,50 +32,19 @@ export default function Profile() {
             </header>
             <h1>Casos cadastrados</h1>
             <ul>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso Teste</p>
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Caso Teste</p>
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3" />
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso Teste</p>
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Caso Teste</p>
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3" />
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso Teste</p>
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Caso Teste</p>
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3" />
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso Teste</p>
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Caso Teste</p>
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3" />
-                    </button>
-                </li>
+                {incidents.map(incident => ( // Foi colocado o parenteses na abertura da arrow function para já retornar o resultado, senão teria que colocar o return () após a {
+                    <li key={incident.id}>
+                        <strong>CASO:</strong>
+                        <p>{incident.title}</p>
+                        <strong>DESCRIÇÃO:</strong>
+                        <p>{incident.description}</p>
+                        <strong>VALOR:</strong>
+                        <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
+                        <button type="button">
+                            <FiTrash2 size={20} color="#a8a8b3" />
+                        </button>
+                    </li>
+                ))}
             </ul>
         </div>
     );
